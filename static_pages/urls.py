5bc9eaee901cbb4a3c0088429bcf25ed348512fb
Alexander <bykovskiyan@gmail.com>
2021-11-04 17:46:56 +0300

    Init static_page app

---
+++ static_pages/urls.py
@@ -0,0 +1,7 @@
+from django.urls import re_path
+
+app_name = 'static_pages'
+
+urlpatterns = [
+
+]
---
+++ szfo/common_models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+
+# Абстрактная модель с полями дат создания и последнего изменения экземпляра модели
+class CommonModel(models.Model):
+
+    created_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата создания',
+    )
+    changed_date = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Дата последнего изменения',
+    )
+
+    class Meta:
+        abstract = True
+
+
+# Абстрактная модель полей SEO для добавления в модели материалов и статичных страниц
+class SEOModel(CommonModel):
+
+    slug = models.SlugField(
+        max_length=255,
+        unique=True,
+        db_index=True,
+        verbose_name='URL',
+    )
+    page_description = models.CharField(
+        max_length=1024,
+        verbose_name='Описание для поисковых систем',
+    )
+
+    class Meta:
+        abstract = True
