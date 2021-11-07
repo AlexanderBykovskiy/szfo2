67198bd462db825b8e4a56048e8b9b6d0ce22dc5
Alexander <bykovskiyan@gmail.com>
2021-11-07 17:39:14 +0300

    Fixed ckeditor

--- szfo/settings.py
+++ szfo/settings.py
@@ -113,7 +113,7 @@ MEDIA_URL = '/media/'
 
 # CKeditor
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'ckeditor', 'ckeditor')
+CKEDITOR_BASEPATH = '/static/ckeditor/ckeditor/'
 
 CKEDITOR_UPLOAD_PATH = 'uploads/'
 
