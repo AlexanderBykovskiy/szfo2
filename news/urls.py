97fc8d52d23408ae6ec6e9b444987c07c2730423
Alexander <bykovskiyan@gmail.com>
2021-12-06 09:32:56 +0300

    Fixed urls

--- news/urls.py
+++ news/urls.py
@@ -6,7 +6,7 @@ app_name = 'news'
 urlpatterns = [
 
     re_path(
-        r'^(?P<slug>[a-z0-9]+(?:-[a-z0-9]+)*/?$)',
+        r'^(?P<slug>[a-z0-9]+(?:-[a-z0-9]+)*)/?$',
         NewsItem.as_view(),
         name='news_item'
     ),
