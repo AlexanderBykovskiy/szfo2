5fcc09a7325833b61dc11130636061d3a41c610b
Alexander <bykovskiyan@gmail.com>
2021-12-09 09:41:22 +0300

    Added migrate

--- partners/models.py
+++ partners/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib import admin
+from django.utils.html import format_html
+from easy_thumbnails.fields import ThumbnailerImageField
+from szfo.common_models import SEOModel
 
-# Create your models here.
+
+# Модель партнеров
+class PartnersModel(SEOModel):
+    header = models.CharField(
+        max_length=255,
+        verbose_name='Название',
+    )
+    logo = ThumbnailerImageField(
+        upload_to='images/partners/',
+        verbose_name='Логотип',
+    )
+    link_url = models.URLField(
+        max_length=512,
+        verbose_name='Ссылка (URL)',
+    )
+
+    def __str__(self):
+        return self.header
+
+    #def get_absolute_url(self):
+    #    return reverse('news:news_item', kwargs={'slug': self.slug})
+
+    @admin.display(description='Ссылка')
+    def link(self):
+        return format_html('<a href="{}">Перейти на сайт партнера</a>', self.link_url)
+
+    class Meta:
+        verbose_name = 'Партнер'
+        verbose_name_plural = 'Партнеры'
+        ordering = ['header']
