5483d8ee2a639481f5f219920e9a1a62f167e9a3
Alexander <bykovskiyan@gmail.com>
2022-01-30 15:15:51 +0300

    added cleanup

--- szfo/settings.py
+++ szfo/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_cleanup.apps.CleanupConfig',
     'static_pages.apps.StaticPagesConfig',
     'feedback.apps.FeedbackConfig',
     'news.apps.NewsConfig',
